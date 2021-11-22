160926eb0e1a006743f4ceea1a8cba6eb9037b53
abhishek-codilar <abhishek.n@codilar.com>
2021-11-22 11:04:15 +0530

    Library System

---
+++ Add/PinGenerate.php
@@ -0,0 +1,36 @@
+
+<html>
+    <head>
+        <link rel="stylesheet" href="PinVerify.css">
+    <style>
+        body {font-family: Arial, Helvetica, sans-serif;background-image: url('menu.jpg');
+             }
+    </style>
+    </head>
+    <body>
+    <form action="PinVerification.php" method="POST">
+    <div class="form">
+        <label for="pin">Enter Pin to Add:</label>
+        <input type="number" name="pin" class="pin"/>
+            <span class="alertNotification">
+            <?php
+            if ($_GET['errorname_']) {
+                $_GET['errorname_'] = "*PIN Is Required";
+                echo $_GET['errorname_'];
+            }
+            ?>
+            </span>
+        <span class="alertNotification">
+            <?php
+            if ($_GET['errorsname_']) {
+                $_GET['errorsname_'] = "*Enter Correct PIN";
+                echo $_GET['errorsname_'];
+            }
+            ?>
+            </span>
+        <div class="button">
+            <button type="submit">Verify</button>
+        </div>
+    </form>
+    </body>
+</html>
